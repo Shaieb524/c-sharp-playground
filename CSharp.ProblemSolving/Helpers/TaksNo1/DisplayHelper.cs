5f1521660e47d3c39f99513e4084cef876ca225b
Joud Shaieb (Tech Unicorn) <joud@cxunicorn.com>
2022-08-10 17:05:46 +0400

    update console

--- CSharp.ProblemSolving/Helpers/TaksNo1/DisplayHelper.cs
+++ CSharp.ProblemSolving/Helpers/TaksNo1/DisplayHelper.cs
@@ -8,6 +8,17 @@ namespace CSharp.ProblemSolving.Helpers.TaksNo1
 {
     public class DisplayHelper
     {
+        public void DisplayControlPanel()
+        {
+            Console.WriteLine("# You need to enter pyramid base length and direction");
+            Console.WriteLine(" ");
+            Console.WriteLine("# Here are your direction control keys");
+            Console.WriteLine("     1 - Rotate left");
+            Console.WriteLine("     2 - Rotate up");
+            Console.WriteLine("     3 - Rotate right");
+            Console.WriteLine("     4 - Rotate down");
+            Console.WriteLine(" ");
+        }
         public void DrawAstriskPyramid(int BaseNo, string direction)
         {
             if (BaseNo % 2 != 0)
@@ -15,7 +26,26 @@ namespace CSharp.ProblemSolving.Helpers.TaksNo1
 
                 switch (direction)
                 {
+                    case "Left":
+                    case "1":
+                        int spcaceCounter = 0;
+                        for (int i = 0; i < BaseNo + 1; i++)
+                        {
+                            int numOfAstrisks = i + 1;
+
+                            if (i <= BaseNo / 2)
+                                Console.WriteLine(new string(' ', (BaseNo / 2) - i) + new string('*', i + 1));
+                            else
+                            {
+                                spcaceCounter++;
+                                Console.WriteLine(new String(' ', spcaceCounter) + new string('*', BaseNo - i));
+                            }
+                        }
+                        break;
+
                     case "Up":
+                    case "2":
+
                         for (int i = 0; i < BaseNo; i++)
                         {
                             int numOfSpaces = (BaseNo - i) / 2;
@@ -25,7 +55,21 @@ namespace CSharp.ProblemSolving.Helpers.TaksNo1
                         }
                         break;
 
+                    case "Right":
+                    case "3":
+                        for (int i = 0; i < BaseNo + 1; i++)
+                        {
+                            int numOfAstrisks = i + 1;
+
+                            if (i <= BaseNo / 2)
+                                Console.WriteLine(new string('*', numOfAstrisks));
+                            else
+                                Console.WriteLine(new string('*', BaseNo - i));
+                        }
+                        break;
+
                     case "Down":
+                    case "4":
                         for (int i = 0; i < BaseNo + 1; i++)
                         {
                             int numOfSpaces = i / 2;
@@ -34,33 +78,7 @@ namespace CSharp.ProblemSolving.Helpers.TaksNo1
                                 Console.WriteLine(new string(' ', numOfSpaces) + new string('*', numOfAstrisks));
                         }
                         break;
-
-                    case "Right":
-                        for (int i = 0; i < BaseNo + 1; i++)
-                        {
-                            int numOfAstrisks = i + 1;
-
-                            if (i <= BaseNo / 2)
-                                Console.WriteLine(new string('*', numOfAstrisks));
-                            else
-                                Console.WriteLine(new string('*', BaseNo - i));
-                        }
-                        break;
-
-                    case "Left":
-                        for (int i = 0; i < BaseNo + 1; i++)
-                        {
-                            int numOfAstrisks = i + 1;
-
-                            if (i <= BaseNo / 2)
-                                Console.WriteLine(new string('*', numOfAstrisks));
-                            else
-                                Console.WriteLine(new string('*', BaseNo - i));
-                        }
-                        break;
                 }
-
-
             }
             else
             {
---
+++ CSharp.ProblemSolving/Helpers/TaksNo1/MainConsole.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.ProblemSolving.Helpers.TaksNo1
+{
+    public class MainConsole
+    {
+        public static void DisplayMainConsole()
+        {
+            Console.WriteLine("Hello!");
+
+            var d = new DisplayHelper();
+            d.DisplayControlPanel();
+
+
+            Console.WriteLine("# Please enter pyramid base length (odd number for better display):");
+            var pyramidBaseLength = Console.ReadLine();
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("# Please enter direction No. or name:");
+            var direction = Console.ReadLine();
+
+            var input = "";
+
+            while (input != "quit")
+            {
+                d.DrawAstriskPyramid(Int32.Parse(pyramidBaseLength), direction);
+                Console.WriteLine("# To repeat please enter 'repeat'.");
+                Console.WriteLine("# To exit please enter 'quit'.");
+                Console.WriteLine(" ");
+
+                input = Console.ReadLine();
+                if (input == "repeat")
+                {
+                    DisplayMainConsole();
+                }
+            }
+
+        }
+    }
+}
