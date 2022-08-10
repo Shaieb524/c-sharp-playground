450d1e67696b46f7a439d6401fdc6265678021b2
Joud Shaieb (Tech Unicorn) <joud@cxunicorn.com>
2022-08-10 17:25:59 +0400

    cln

--- CSharp.ProblemSolving/Helpers/TaksNo1/DisplayHelper.cs
+++ CSharp.ProblemSolving/Helpers/TaksNo1/DisplayHelper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CSharp.ProblemSolving.Helpers.TaksNo1
 {
     public class DisplayHelper
