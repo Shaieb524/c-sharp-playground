cc754f5f553b89b97edf3a7e5ed57c829870c1c4
Joud Shaieb <joud@cxunicorn.com>
2022-08-16 11:01:15 +0400

    edit events

--- CSharp.ProblemSolving/Program.cs
+++ CSharp.ProblemSolving/Program.cs
@@ -1,19 +1,3 @@
-﻿using CSharp.ProblemSolving.Tasks.TaskNo2.Delegates;
+﻿using CSharp.ProblemSolving.Tasks.TaskNo2.Events;
 
-Document doc = new Document();
-
-PostBlog blogPoster = new PostBlog();
-
-// instantiate delegate from document
-var blogDelegate = new Document.SendDoc(blogPoster.DeliverPost);
-doc.DocumentSender(blogDelegate);
-
-// or pass it directly
-doc.DocumentSender(blogPoster.DeliverPost);
-
-EmailSender emailSender = new EmailSender();
-var emailDelegate = new Document.SendDoc(emailSender.SendEmail);
-doc.DocumentSender(emailDelegate);
-doc.DocumentSender(emailSender.SendEmail);
-
-
+TestEvent.Main();
---
+++ CSharp.ProblemSolving/Tasks/TaskNo2/Events/BankAccount/TestEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.ProblemSolving.Tasks.TaskNo2.Events.BankAccount
+{
+    public class TestEvent
+    {
+        public static void SendNotification(object sender, TransactionEventArgs e)
+        {
+            Console.WriteLine("You have been {0} with {1}", e.TransactionType, e.TransactionAmout);
+        }
+        public static void Main()
+        {
+            Account acc = new Account(1000);
+            acc.TransactionMade += new TransactionHandler(SendNotification);
+            acc.Credit(500);
+            Console.WriteLine("Your current account is {0}", acc.Balance);
+        }
+    }
+}
